55e0c297fcc71ca0437d9137301f5ec88915b49c
Sieun <si990819@gmail.com>
2023-08-20 18:40:24 +0900

    cardbox

---
+++ cardNews.test/cardnews.js
@@ -0,0 +1,11 @@
+//header에 각 요소에 값을 주고
+//onclick이 했을때 그 값을 갖고온다
+
+//그다음 미리 만들어진 components를 셋팅해놓고
+//클릭했을때의 값과 components의 값이 같으면
+//createElement로 forms에 넣어준다
+//물론 값이 다르면 지우는 기능도 필요하겠다
+
+// const new_cardnews1 = document.getElementById('new_cardnews1');
+
+
---
+++ homePage/cardNews/cardnews.js
@@ -0,0 +1,121 @@
+//클릭이 어디로 되어있나 기준으로 할껀데
+// const news
+const A = document.getElementById('A');
+const B = document.getElementById('B');
+const C = document.getElementById('C');
+const D = document.getElementById('D');
+const E = document.getElementById('E');
+
+
+function typeSearch(type) {
+    let idValue = type
+    if (idValue == "news" || idValue == "mediaEdit" ||
+    idValue == "entertain"||idValue == "sports" || idValue == "economics") {
+        console.log("제대로 선택함")
+        cardSelect(idValue);
+        
+    }
+    else {
+        console.log("관리자에게 문의하셈")
+    }
+}
+
+function test(type) {
+    if (type == "A") {
+       console.log("버튼 A 입니다");
+    } else if (type == "B") {
+        console.log("버튼 B 입니다");
+    } else if (type == "C") {
+        console.log("버튼 C 입니다");
+    } else if (type == "D") {
+        console.log("버튼 D 입니다");
+    } else if (type == "E") {
+        console.log("버튼 E 입니다");
+    }
+}
+
+function cardSelect(type) {
+    console.log("안뇽");
+    console.log(A);
+    console.log(B);
+    console.log(C);
+    console.log(D);
+    console.log(E);
+    // A.style.pointerEvents="none";
+    if (type == "news") {
+        A.style.pointerEvents = "auto";
+        B.style.pointerEvents = "none";
+        C.style.pointerEvents = "none";
+        D.style.pointerEvents = "none";
+        E.style.pointerEvents = "none";
+        cardDisplay(type);
+    }else if(type=="mediaEdit") {
+        A.style.pointerEvents = "none";
+        B.style.pointerEvents = "auto";
+        C.style.pointerEvents = "none";
+        D.style.pointerEvents = "none";
+        E.style.pointerEvents = "none";
+        cardDisplay(type);
+    }else if (type == "entertain") {
+        A.style.pointerEvents = "none";
+        B.style.pointerEvents = "none";
+        C.style.pointerEvents = "auto";
+        D.style.pointerEvents = "none";
+        E.style.pointerEvents = "none";
+        cardDisplay(type);
+    } else if (type == "sports") {
+        A.style.pointerEvents = "none";
+        B.style.pointerEvents = "none";
+        C.style.pointerEvents = "none";
+        D.style.pointerEvents = "auto";
+        E.style.pointerEvents = "none";
+        cardDisplay(type);
+    } else if (type == "economics") {
+        A.style.pointerEvents = "none";
+        B.style.pointerEvents = "none";
+        C.style.pointerEvents = "none";
+        D.style.pointerEvents = "none";
+        E.style.pointerEvents = "auto";
+        cardDisplay(type);
+    }
+}
+function cardDisplay(type){
+    if (type == "news") {
+        A.style.display = "block";
+        B.style.display = "none";
+        C.style.display = "none";
+        D.style.display = "none";
+        E.style.display = "none";
+
+    } else if(type=="mediaEdit") {
+        A.style.display = "none";
+        B.style.display = "block";
+        C.style.display = "none";
+        D.style.display = "none";
+        E.style.display = "none";
+       
+    }else if (type == "entertain") {
+        A.style.display = "none";
+        B.style.display = "none";
+        C.style.display = "block";
+        D.style.display = "none";
+        E.style.display = "none";
+    } else if (type == "sports") {
+        A.style.display = "none";
+        B.style.display = "none";
+        C.style.display = "none";
+        D.style.display = "block";
+        E.style.display = "none";
+    } else if (type == "economics") {
+        A.style.display = "none";
+        B.style.display = "none";
+        C.style.display = "none";
+        D.style.display = "none";
+        E.style.display = "block";
+    }
+}
+
+
+
+
+
